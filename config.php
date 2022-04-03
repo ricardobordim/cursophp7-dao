4425992f91c55d0f7e76cd487f288c5b22fb9647
Ricardo Bordim <rbordim@gmail.com>
2022-04-03 15:37:16 -0300

    Metodos de Lista

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
+
 spl_autoload_register(function($class_name){
-    $filename = "class".DIRECTORY_SEPARATOR. $class_name . ".php";
-    if(file_exists($filename)){
+
+    $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
+
+    if (file_exists(($filename))) {
+
         require_once($filename);
+
     }
+
 });
 
 ?>
--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(1);
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//  $search = Usuario::search("jo");
+//  echo json_encode($search);
 
-//Primeira Etapa 
-// $sql = new Sql();
-// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-// echo json_encode($usuarios);
+//Carrega um usuário usando o login e a senha
+  $usuario = new Usuario();
+  $usuario->login("joao", "qwerty");
 
+ echo $usuario;
 
 ?>
